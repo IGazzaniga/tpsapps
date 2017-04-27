0b4623fc1610f4f17ebf6b1198c8eef69be670e0
IGazzaniga <ignagazzaniga@hotmail.com.ar>
2017-04-27 16:24:49 -0300

    Punto 1 terminado

--- js/validacion.js
+++ js/validacion.js
@@ -1,74 +1,7 @@
-/*function validar() {
-	var letras = /^[A-Za-z]+$/;
-    var x = document.forms["formulario"]["nombre"].value;
-    var mensaje;
-
-    if (x == "" || !/^[a-zA-Z]*$/g.test(x)) {
-        alert("El campo nombre no debe ser vacío ni contener números");
-      }
-      else{
-      	mensaje = "Nombre: "+x+"\n";
-       }
-    
-    var x = document.forms["formulario"]["apellido"].value;
-
-    if (x == "" || !/^[a-zA-Z]*$/g.test(x)) {
-        alert("El campo apellido no debe ser vacío ni contener números");
-      }
-      else{
-      	mensaje = mensaje+"Apellido: "+x +"\n";
-      }
-
-    var x = document.forms["formulario"]["fecha"].value;
-
-    if (x == "") {
-        alert("Debe completar el fecha");	
-    }
-    else{
-    	var f = x.toString();
-    	mensaje = mensaje+"Fecha: "+f+"\n";
-    	
-    }
-
-    var x = document.forms["formulario"]["sexo"].value;
-
-    if (x == "") {
-        alert("Debe completar el sexo");
-    }
-    else{
-    	mensaje = mensaje+"Sexo: "+x+"\n";
-
-    }
-    
-    var x = document.forms["formulario"]["valoracion"].value;
-
-    if (x == "") {
-        alert("Debe completar el valoración");
-    }
-    else{
-    	mensaje = mensaje+"Valoracion: "+x;
-    	alert(mensaje);
-    }
-
-    return false;
-
-    
-
-  }
-
-  function cancelar(){
-  	var r = confirm("¿Desea cancelar? Si lo hace, volvera al inicio. Pulse OK para cancelar o Cancelar para deshacer");
-    if (r == true) {
-    	window.location.href = './index.html';
-        
-    } else {
-        
-    }
-  }
-*/
   function validar() {
-  var letras = /^[A-Za-z]+$/;
-    var x = document.forms["formulario"]["nombre"].val();
+    var letras = /^[A-Za-z]+$/;
+    var x = $("input[name=nombre]").val();
+    var y = 0;
     var mensaje;
 
     if (x == "" || !/^[a-zA-Z]*$/g.test(x)) {
@@ -76,48 +9,56 @@
       }
       else{
         mensaje = "Nombre: "+x+"\n";
+        y++;
        }
     
-    var x = document.forms["formulario"]["apellido"].val();
+    var x = $("input[name=apellido]").val();
 
     if (x == "" || !/^[a-zA-Z]*$/g.test(x)) {
         alert("El campo apellido no debe ser vacío ni contener números");
       }
       else{
         mensaje = mensaje+"Apellido: "+x +"\n";
+        y++;
       }
 
-    var x = document.forms["formulario"]["fecha"].val();
+    var x = $("input[name=fecha]").val();
 
     if (x == "") {
-        alert("Debe completar el fecha"); 
+        alert("Debe completar la fecha"); 
     }
     else{
       var f = x.toString();
       mensaje = mensaje+"Fecha: "+f+"\n";
-      
+      y++;
     }
-
-    var x = document.forms["formulario"]["sexo"].val();
-
-    if (x == "") {
+    
+    var x = $("input[name=sexo]:checked").val();
+      if (x == undefined) {
         alert("Debe completar el sexo");
     }
     else{
       mensaje = mensaje+"Sexo: "+x+"\n";
-
+      y++;
     }
     
-    var x = document.forms["formulario"]["valoracion"].val();
-
-    if (x == "") {
-        alert("Debe completar el valoración");
+    var x = $("input[name=valoracion]:checked").val();
+      if (x == undefined) {
+        alert("Debe completar la valoración");
     }
     else{
       mensaje = mensaje+"Valoracion: "+x;
-      alert(mensaje);
+      y++;
     }
+    if (y==5){
+    alert(mensaje);
+    $("#formulario").fadeOut(1000);
+    $("#oculto").fadeIn(1000);
+    $("#oculto").fadeOut(5000);
+    $("#formulario").fadeIn(6000);
+    $('#formulario')[0].reset();
 
+    }
     return false;
 
     
